42ac894a35cc59589fe182747200972a1958fcdb
Amit Pal <pal032209@gmail.com>
2026-09-06 22:25:43 +0530

    sidebar create

--- Fronted/src/component/Layout.jsx
+++ Fronted/src/component/Layout.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, {useState } from 'react'
 import {styles} from '../assets/dummystyle'
 import Navbar from './Navbar'
+import Sidebar from './sidebar'
+
 
 const Layout = ({onLogout,user}) => {
+  const[sidebarCollapsed,setSidebarCollapsed] = useState(false);
   return (
+    
     <div className={styles.layout.root}>
     <Navbar user={user} onLogout={onLogout}/>
+     <Sidebar user={user} isCollapsed={sidebarCollapsed} setIsCollapsed ={setSidebarCollapsed}/>
     </div>
   )
 }
